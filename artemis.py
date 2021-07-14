722c1441c67433f4381b007645e6855e96aed143
farrukh.sadykov <farrukh.sadykov@capgemini.com>
2021-07-14 17:39:20 -0500

    added version 7

--- artemis.py
+++ artemis.py
@@ -8,11 +8,19 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
 
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
